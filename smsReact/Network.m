0ad1ea1ef6f8ebd0bbd4a102f29e0909125f0cc3
C191239 <zsz2007314zsz@sina.com>
2023-12-09 23:13:09 +0800

    found way

--- smsReact/Network.m
+++ smsReact/Network.m
@@ -6,30 +6,46 @@
 //
 
 #import "Network.h"
+#include <Foundation/Foundation.h>
 
-@interface Network()
-@property (strong) NSString *baseAddr;
+@interface Network ()
+@property(strong) NSString *baseAddr;
 @property NSUInteger basePort;
-@property (strong) NSString *userToken;
+@property(strong) NSString *userToken;
 @property NSUInteger userId;
-@property (strong) NSURLSession *session;
+@property(strong) NSURLSession *session;
 @end
 @implementation Network
 
-- (instancetype)initWithAddr:(NSString *)addr Port:(NSUInteger)port
-{
-    self=[super init];
-    _baseAddr=addr;
-    _basePort=port;
-    _session=[NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration ephemeralSessionConfiguration]];
-    return self;
+- (instancetype)initWithAddr:(NSString *)addr Port:(NSUInteger)port {
+  self = [super init];
+  _baseAddr = addr;
+  _basePort = port;
+  _session = [NSURLSession
+      sessionWithConfiguration:[NSURLSessionConfiguration
+                                   ephemeralSessionConfiguration]];
+  return self;
 }
-- (void)loginWithEmail:(NSString *)email Password:(NSString *)password{
-    NSMutableURLRequest *request=[NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@/api/account/login",_baseAddr]]];
-    NSDictionary *dict=[NSDictionary dictionaryWithObjectsAndKeys:email, @"email", password, @"password", nil];
-    NSData *data=[NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingSortedKeys error:nil];
-    NSURLSessionUploadTask *task=[_session uploadTaskWithRequest:request fromData:data];
-    task.taskDescription=@"login";
-    [task resume];
+- (void)loginWithEmail:(NSString *)email Password:(NSString *)password {
+  NSMutableURLRequest *request = [NSMutableURLRequest
+      requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:
+                                                        @"%@/api/account/login",
+                                                        _baseAddr]]];
+  NSDictionary *dict = [NSDictionary
+      dictionaryWithObjectsAndKeys:email, @"email", password, @"password", nil];
+  NSData *data = [NSJSONSerialization dataWithJSONObject:dict
+                                                 options:nil
+                                                   error:nil];
+  NSURLSessionUploadTask *task = [_session uploadTaskWithRequest:request
+                                                        fromData:data];
+  task.taskDescription = @"login";
+  [task resume];
+}
+- (void)URLSession:(NSURLSession *)session
+                    task:(NSURLSessionTask *)task
+    didCompleteWithError:(NSError *)error {
+  if ([session isEqualTo:_session]) {
+    switch ([task.taskDescription intValue]) {}
+  }
 }
 @end
